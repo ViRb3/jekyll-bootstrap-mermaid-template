f0bc268d71a221dc986ac4c05e7c5776c371900d
Victor <virb3e@gmail.com>
2019-11-06 21:10:49 +0000

    Add mermaid

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
-# A useful script to download the latest version of bootstrap and jquery
+# A useful script to download the latest version of bootstrap, jquery and mermaid
 
 rm -rf node_modules package-lock.json
-npm install bootstrap@4 jquery@3 popper.js@^1.14.7
+npm install bootstrap@4 jquery@3 popper.js@^1.14.7 mermaid@^8.4.1
 
 rm -rf _sass/bootstrap
 mkdir -p _sass/bootstrap
@@ -12,3 +12,7 @@ rm -rf assets/javascript/bootstrap
 mkdir -p assets/javascript/bootstrap
 cp node_modules/bootstrap/dist/js/bootstrap.bundle.min.* assets/javascript/bootstrap/
 cp node_modules/jquery/dist/jquery.min.* assets/javascript/bootstrap/
+
+rm -rf assets/javascript/mermaid
+mkdir -p assets/javascript/mermaid
+cp node_modules/mermaid/dist/mermaid.min.* assets/javascript/mermaid/
